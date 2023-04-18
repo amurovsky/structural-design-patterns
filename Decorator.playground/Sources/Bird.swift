b499ce7cb2e84051df9b4086d246435f08b09828
Esteban Garibay <esteban.garibay@wizeline.com>
2023-04-18 17:52:08 -0600

    | public initializers |

--- Decorator.playground/Sources/Bird.swift
+++ Decorator.playground/Sources/Bird.swift
@@ -1,6 +1,8 @@
 public class Bird {
     public var age = 0
 
+    public init() {}
+
     public func fly() -> String {
         return (age < 10) ? "flying" : "too old"
     }
--- Decorator.playground/Sources/Lizard.swift
+++ Decorator.playground/Sources/Lizard.swift
@@ -1,6 +1,8 @@
 public class Lizard {
     public var age = 0
 
+    public init() {}
+
     public func crawl() -> String {
         return (age > 1) ? "crawling" : "too young"
     }
